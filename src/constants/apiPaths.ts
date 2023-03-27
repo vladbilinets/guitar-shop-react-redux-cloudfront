7a0d9190e79e955cb9b0f0738239abc5e1cfa885
Vladyslav Bilynets <vladyslav.bilynets@vizio.com>
2023-03-27 17:49:18 +0200

    config update

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-central-1.amazonaws.com/dev",
+  product: "https://l789itwdsl.execute-api.eu-central-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-central-1.amazonaws.com/dev",
   import: "https://15q3yszh8e.execute-api.eu-central-1.amazonaws.com/dev",
-  bff: "https://6qwfcs9ay0.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://l789itwdsl.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-central-1.amazonaws.com/dev",
 };
 
